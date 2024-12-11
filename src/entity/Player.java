50b04cebdc807fbc81cd0ba9bebdb78a6cba5990
Shaun <scpour@gmail.com>
2024-12-10 22:41:17 -0500

    stuff

---
+++ src/entity/Player.java
@@ -0,0 +1,13 @@
+package entity;
+
+import Main.*;
+
+public class Player extends Entity {
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public Player(GamePanel gp, KeyHandler keyH) {
+        this.gp = gp;
+        this.keyH = keyH;
+    }
+}
